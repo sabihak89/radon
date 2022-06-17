8d53cfce29947fdad27dae1782073f70f06e5d03
KISHAN SINGH 8887969978 <106567232+Kishansingh2002@users.noreply.github.com>
2022-06-17 14:31:45 +0530

    Add files via upload
    auth part2 and tra/catch

---
+++ src/routes/route.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const userController = require("../controllers/userController")
+const mid=require("../middleware/auth")
+
+router.get("/test-me", function (req, res) {
+    res.send("My first ever api!")
+});
+
+router.post("/users", userController.createUser)
+
+router.post("/login", userController.loginUser)
+
+router.get("/users/:userId", mid.middle1, userController.getUserData)
+
+router.put("/users/:userId", mid.middle1, mid.middle2, userController.updateUser)
+
+router.delete("/users/:userId", mid.middle1, mid.middle2, userController.deleteUser)
+
+module.exports = router;
+
+
+
+
+
+
+module.exports = router;
