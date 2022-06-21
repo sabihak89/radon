da6ae5ace04c1424e0df8e224dae3b07c5f6d0b4
Kaluram <kaluramkanesh123@gmail.com>
2022-06-22 00:13:25 +0530

    try and catch add

--- src/controller/authorController.js
+++ src/controller/authorController.js
@@ -3,9 +3,15 @@ const modelBlog = require("../model/blogModel")
 
 
 const createAuthor = async function (req, res) {
-    let authorData = req.body
-    let showAuthorData = await model.create(authorData)
-    res.send(showAuthorData)
+
+    try {
+        let authorData = req.body
+        let showAuthorData = await model.create(authorData)
+        res.status(201).send(showAuthorData)
+    }
+    catch (err) {
+        res.status(500).send({ status: false, msg: err })
+    }
 }
 
-module.exports.createAuthor=createAuthor
+module.exports.createAuthor = createAuthor
