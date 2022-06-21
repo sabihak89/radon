dbb43945484e8da986e7ce2971b40358c0b885eb
rekhachamakeri <rekhagchamakeri@gmail.com>
2022-06-21 13:38:48 +0530

    blogSchema

---
+++ src/blogModel.js
@@ -0,0 +1,37 @@
+const { ObjectId } = require('bson');
+const mongoose = require('mongoose');
+
+const blogSchema = new mongoose.Schema( {
+    title: {
+        type: String, 
+        required: true
+    },
+    body:{
+        type: String,
+        required: true
+    },
+    authorId:{
+        type:ObjectId, 
+        ref:author1
+    },
+    tags:{
+        type: Array,
+    },
+    category:{
+        type:String,
+        required:true
+    },
+    subcategory: {
+        type:String},
+    isDeleted: {
+     type:Boolean,
+     default: false},
+    isPublished: {
+     type:Boolean,
+     default: false
+    }
+
+}, { timestamps: true });
+
+
+module.exports = mongoose.model('blog', blogSchema) 
